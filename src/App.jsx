7471173aad10801e8b11359798ecc2a1eb4dd069
Felipe Santana <felipe012santana012@gmail.com>
2024-06-17 16:23:24 -0300

    Started project and initial configurations

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+
+
+  return (
+   <div className='app'>
+      <NavBar/>
+    <div className='container'>
+      <Outlet/>
+    </div>
+   </div>
+  )
+}
+
+export default App
